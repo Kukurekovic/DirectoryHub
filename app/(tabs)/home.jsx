5a5dbb716d4d8ea8d3af2d8349e431e5965d5d1e
Kukurekovic <kukurekovicmarko@gmail.com>
2024-06-30 22:38:38 +0200

    7. commit

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,13 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import Header from "../../components/Home/Header";
 import Slider from "../../components/Home/Slider";
 import Category from "../../components/Home/Category";
+import PopularBusiness from "../../components/Home/PopularBusiness";
 
 export default function home() {
   return (
-    <View>
+    <ScrollView>
 
       {/* HEADER */}
       <Header />
@@ -18,6 +19,9 @@ export default function home() {
       <Category />
 
       {/* POPULAR BUSINESS LIST */}
-    </View>
+      <PopularBusiness />
+
+      <View style={{height: 50}}></View>
+    </ScrollView>
   );
 }
